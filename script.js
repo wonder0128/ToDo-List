b0409cbb3258026e0753dc2e843316b9834673c0
roze <wonder_0128@naver.com>
2024-10-22 22:17:45 +0900

    1022-scope&hoisting -1

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-const checkInput = function() {
-  const inputValue = document.querySelector('#todo-input').value;
-  console.log(inputValue)
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.getElementById('todo-list');
 
+const createTodo = function() {
+  const newLi = document.createElement('li');
+  const newSpan = document.createElement('span'); 
+  const newBtn = document.createElement('button');
+
+  newBtn.addEventListener('click', () => {
+    newLi.classList.toggle('completed');
+  })
+
+  newLi.addEventListener('dblclick', () => {
+    newLi.remove();
+  })
+
+  newSpan.textContent = todoInput.value;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+
+  todoInput.value = '';
+  saveItemsFn();
+}
+
+const keyCodeCheck = function() {
+  if(window.event.keyCode === 13 && todoInput.value !== '') {
+    createTodo();
+  }
+}
+
+const deleteAll = function() {
+  const liList = document.querySelectorAll('li');
+  liList.forEach(li => {
+    li.remove();
+  })
+}
+
+const saveItemsFn = function() {
+  const saveItems = [];
+  const todo = todoList.querySelectorAll('li');
+  
+  todo.forEach(li => {
+    const todoObj = {
+      contents: li.querySelector('span').textContent,
+      completed: li.classList.contains('completed')
+    }
+    saveItems.push(todoObj);
+  })
+  console.log(saveItems);
 }
