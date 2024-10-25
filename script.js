e1824272f3b3a4fc9381fdc99e9509674baaf209
roze <110625211+wonder0128@users.noreply.github.com>
2024-10-25 17:25:33 +0900

    1025 - 구조분해할당 진행중

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const createTodo = function(storageData) {
 }
 
 const keyCodeCheck = function() {
-  if(window.event.keyCode === 13 && todoInput.value !== '') {
+  if(window.event.keyCode === 13 && todoInput.value.trim() !== '') {
     createTodo();
   }
 }
